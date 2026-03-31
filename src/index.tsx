c0c00df6168dd7dea905f9f134a8892a1939f35d
gustavo <gustavoalmeida5446@gmail.com>
2026-03-31 15:47:42 -0300

    requested changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/react_tabs-with-router">
     <App />
   </BrowserRouter>,
 );
